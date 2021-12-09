741514f669ecc553a6e453da0b96b70344539099
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2021-12-09 16:17:32 +0700

    sd

--- GENEALOGY/genealogy_tree/models.py
+++ GENEALOGY/genealogy_tree/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+    SEX_CHOICES = [
+        ('male', 'Nam'),
+        ('female', 'Nữ'),
+    ]
+    sex = models.CharField(max_length=6, choices=SEX_CHOICES)
+    birth_date = models.DateField(blank=True)
+
+class FamilyLine(models.Model):
+    sur_name = models.CharField(max_length=200)
+    
+class Address(models.Model):
+    province_id = models.IntegerField()
+    district_id = models.IntegerField()
+    commune_id = models.IntegerField()
+
+
